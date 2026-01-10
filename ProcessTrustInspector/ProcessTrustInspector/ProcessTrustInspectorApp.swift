1095c22c381def29eb101d6bade8d59919060b86
Aaron Weiss <28325936+friendlysapphire@users.noreply.github.com>
2026-01-10 09:27:33 -0800

    Add initial macOS app skeleton (SwiftUI)

---
+++ ProcessTrustInspector/ProcessTrustInspector/ProcessTrustInspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProcessTrustInspectorApp.swift
+//  ProcessTrustInspector
+//
+//  Created by Aaron Weiss on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ProcessTrustInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
